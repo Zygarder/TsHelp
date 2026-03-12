88d7ef47c7e9a8d15c0eda7da1c8bf5ce97b8560
Louie <louieguiritan92@gmail.com>
2026-03-12 14:52:58 +0800

    Start point

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Welcome to TSHelp</h1>
+      <p>The centralized ticketing system is up and running!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App'
+  }
+  </script>
---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue'; // We will create this file next
+
+createApp(App).mount('#app');
